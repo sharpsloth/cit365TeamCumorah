ad194ff3aaf619aab91ff70d1210bc9edfc0954a
laiyam <laibyui@gmail.com>
2020-07-17 20:35:54 -0400

    Added Print function to Program Planner and Program Details

--- SacramentMeetingPlanner/SacramentMeetingPlanner/wwwroot/js/site.js
+++ SacramentMeetingPlanner/SacramentMeetingPlanner/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function printIt(printArea) {
+    var printContents = document.getElementById("printArea").innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
