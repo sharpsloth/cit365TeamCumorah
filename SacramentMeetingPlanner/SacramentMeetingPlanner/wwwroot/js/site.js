c00304b9aba1dd43883ac1cca986a1422eaf9c4f
Jose Concha <joseconchan1@gmail.com>
2020-07-21 21:49:10 -0400

    Dynamic add Speakers Create

--- SacramentMeetingPlanner/SacramentMeetingPlanner/wwwroot/js/site.js
+++ SacramentMeetingPlanner/SacramentMeetingPlanner/wwwroot/js/site.js
@@ -17,3 +17,27 @@ function printIt(printArea) {
 
     document.body.innerHTML = printPage;
 }
+
+function addSpeaker() {
+    var nSpeakers = document.querySelectorAll('.speaker')
+    if (nSpeakers == null) {
+        nSpeakers = 0
+    } else {
+        nSpeakers = nSpeakers.length
+    }
+
+    var template = ''
+    template += '<div class="speaker" >'
+    template += '<div class="form-group">'
+    template += '<label class="control-label">Speaker Name</label>'
+    template += '<input class="form-control" name="Speakers[' + nSpeakers+'].Name" />'
+    template += '</div >'
+    template += '<div class="form-group">'
+    template += '<label class="control-label">Speaker Subject</label>'
+    template += '<input class="form-control" name="Speakers[' + nSpeakers +'].Subject" />'
+    template += '</div>'
+    template += '</div>'
+
+    //document.getElementById('addSpeakerTarget').innerHTML = template
+    $('#addSpeakerTarget').append(template)
+}
