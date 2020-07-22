45705f34247b24097c50de31c1198269c2566460
Clarisse Mendenhall <clarissemendenhall@gmail.com>
2020-07-22 18:50:43 -0500

    deleting the required validation on the speakers

---
+++ SacramentMeetingPlanner/SacramentMeetingPlanner/Migrations/20200722233003_Initial.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SacramentMeetingPlanner.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Speakers_SacramentMeeting_SacramentMeetingId",
+                table: "Speakers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "Speakers",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SacramentMeetingId",
+                table: "Speakers",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Speakers",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SacramentHymn",
+                table: "SacramentMeeting",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OpeningSong",
+                table: "SacramentMeeting",
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OpeningPrayer",
+                table: "SacramentMeeting",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IntermediateNumber",
+                table: "SacramentMeeting",
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConductingLeader",
+                table: "SacramentMeeting",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClosingSong",
+                table: "SacramentMeeting",
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClosingPrayer",
+                table: "SacramentMeeting",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Speakers_SacramentMeeting_SacramentMeetingId",
+                table: "Speakers",
+                column: "SacramentMeetingId",
+                principalTable: "SacramentMeeting",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Speakers_SacramentMeeting_SacramentMeetingId",
+                table: "Speakers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "Speakers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SacramentMeetingId",
+                table: "Speakers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Speakers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SacramentHymn",
+                table: "SacramentMeeting",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 40);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OpeningSong",
+                table: "SacramentMeeting",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 40,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OpeningPrayer",
+                table: "SacramentMeeting",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 40);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IntermediateNumber",
+                table: "SacramentMeeting",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 40,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConductingLeader",
+                table: "SacramentMeeting",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 40);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClosingSong",
+                table: "SacramentMeeting",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 40,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClosingPrayer",
+                table: "SacramentMeeting",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 40);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Speakers_SacramentMeeting_SacramentMeetingId",
+                table: "Speakers",
+                column: "SacramentMeetingId",
+                principalTable: "SacramentMeeting",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
