badb4bb939276d710672767daef601deadc8391b
Ch3my <35230589+Ch3my@users.noreply.github.com>
2020-07-22 19:22:23 -0400

    Validation Message

--- SacramentMeetingPlanner/SacramentMeetingPlanner/wwwroot/js/site.js
+++ SacramentMeetingPlanner/SacramentMeetingPlanner/wwwroot/js/site.js
@@ -30,11 +30,15 @@ function addSpeaker() {
     template += '<div class="speaker" >'
     template += '<div class="form-group">'
     template += '<label class="control-label">Speaker Name</label>'
-    template += '<input class="form-control" name="Speakers[' + nSpeakers+'].Name" />'
+    template += '<input class="form-control" name="Speakers[' + nSpeakers + '].Name" />'
+    template += '<span asp - validation -for= "ClosingPrayer" class= "text-danger" ></span>'
+
     template += '</div >'
     template += '<div class="form-group">'
     template += '<label class="control-label">Speaker Subject</label>'
-    template += '<input class="form-control" name="Speakers[' + nSpeakers +'].Subject" />'
+    template += '<input class="form-control" name="Speakers[' + nSpeakers + '].Subject" />'
+    template += '<span asp - validation -for= "ClosingPrayer" class= "text-danger" ></span>'
+
     template += '</div>'
     template += '</div>'
 
